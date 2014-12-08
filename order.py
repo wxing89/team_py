152b6b9d28e9d3e56e4ccc852be68cfd5888e243
wxing89 <wxing89@163.com>
2014-12-08 15:05:05 +0800

    nothing

--- order.py
+++ order.py
@@ -1,79 +1,39 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#__
-
-import os
-import sys
-import re
-import similarity
-import pickle
+# -*- coding:utf8 ---
 
 class Order():
     """
-    userSim: user similarity matrix, like
-        {userA: [(userB, 0.56), (userC, 0.38) ...]}
-        {userB: [(userD, 0.76), (userA, 0.56) ...]}
+    user_item: user-item matrix
+        {userA: (item1, item2, item3, ...)}
+        {userB: (item2, item4, item5, ...)}
         ...
 
-    userItem: user - item matrix, like
-        {userA: (item1, item2, item3)}
-        {userB: (item2, item4, item5)}
-        ...
+    item_user: item-user matrix
+        {itemA: (user1, user2, user3, ...)}
+        {itemB: (user2, user4, user5, ...)}
     """
-    def __init__(self, size=20):
-        self.simSize = size
-        self.userSim = {}
-        self.userItem = {}
+
+    def __init__(self, *details):
+        self.user_item = {}
+        self.item_user = {}
+        for user, item in details:
+            self.addOrder(user, item)
+
+
+    def addOrder(self, user, item):
+        self.addUser(user, item)
+        self.addItem(user, item)
+
+
+    def addUser(self, user, item):
+        _i_u = self.item_user
+
+        _i_u[item] = _i_u.get(item, set())
+        _i_u[item].add(user)
+
 
     def addItem(self, user, item):
-        self.userItem[user] = self.userItem.get(user, set())
-        self.userItem[user].add(item)
+        _u_i = self.user_item
 
-    def getSim(self):
-        for u1 in self.userItem.keys():
-            for u2 in self.userItem.keys():
-                if u1 is u2:
-                    continue
-                sim = similarity.jaccardIndex(self.userItem[u1], self.userItem[u2])
-                self.userSim[u1] = self.userSim.get(u1, [])
-                if len(self.userSim[u1]) < self.simSize:
-                    self.userSim[u1].append((u2, sim))
-                else:
-                    if sim > self.userSim[u1][self.simSize - 1][1]:
-                        self.userSim[u1].append((u2, sim))
-                        self.userSim[u1].sort(key=lambda x:x[1], reverse=True)
-                        self.userSim[u1].pop()
-
-def main():
-    order = Order()
-
-    orderFile = 'D:\\user\\order\\order201408.txt'
-    f_order = open(orderFile)
-    for line in f_order:
-        temp = line.split('|')
-        user, item = temp[8].strip(), temp[7].strip()
-        order.addItem(user, item)
-
-    order.getSim()
-
-    i = 0
-    for user, item in order.userItem.items():
-        if i < 20:
-            print user, item
-            i += 1
-        else:
-            break
-
-    i = 0
-    for user, sim in order.userSim.items():
-        if i < 20:
-            print user, sim
-            i += 1
-        else:
-            break
-
-    with open('order.dump', 'w') as f:
-        pickle.dump(order, f)
-
-if __name__ == '__main__':
-    main()
+        _u_i[user] = _u_i.get(user, set())
+        _u_i[user].add(item)
